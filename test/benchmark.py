95d3bcdbf416a123c26c04d878dca55233b2ac29
jeppe-biolib <>
2021-04-22 15:32:54 +0200

    update

--- test/benchmark.py
+++ test/benchmark.py
@@ -2,26 +2,30 @@ import pandas as pd
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--predictions')
-parser.add_argument('--actual')
-args = parser.parse_args()
+def benchmark(predictions_file, actuals_file):
+    predictions = pd.read_csv(predictions_file).to_dict(orient="records")
+    actuals = pd.read_csv(actuals_file).to_dict(orient="records")
 
-predictions = pd.read_csv(args.predictions).to_dict(orient="records")
-actuals = pd.read_csv(args.actual).to_dict(orient="records")
+    predictions_dict = {}
+    for prediction in predictions:
+        predictions_dict[prediction["name"]] = prediction["prediction"]
 
-predictions_dict = {}
-for prediction in predictions:
-    predictions_dict[prediction["name"]] = prediction["prediction"]
+    actuals_dict = {}
+    for actual in actuals:
+        actuals_dict[actual["name"]] = actual["actual"]
 
-actuals_dict = {}
-for actual in actuals:
-    actuals_dict[actual["name"]] = actual["actual"]
+    results = []
+    for (name, actual) in actuals_dict.items():
+        results.append(1 if predictions_dict[name] == actual else 0)
 
-results = []
-for (name, actual) in actuals_dict.items():
-    results.append(1 if predictions_dict[name] == actual else 0)
+    accuracy = np.array(results).mean()
+    result = {}
+    result["accuracy"] = accuracy
+    return result
 
-accuracy = np.array(results).mean()
-
-print("Model Accuracy:", accuracy)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--predictions')
+    parser.add_argument('--actual')
+    args = parser.parse_args()
+    print("Benchmarks:", benchmark(args.predictions, args.actual))
