5c6c04abb8bc737b85c8bd095106ffa6c98e335f
jeppe-biolib <>
2021-04-22 13:53:05 +0200

    add benchmark code

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import numpy as np
+import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--predictions')
 parser.add_argument('--actual')
 args = parser.parse_args()
 
-predictions = pd.read_csv(args.predictions)
-actual = pd.read_csv(args.actual)
+predictions = pd.read_csv(args.predictions).to_dict(orient="records")
+actuals = pd.read_csv(args.actual).to_dict(orient="records")
 
+predictions_dict = {}
+for prediction in predictions:
+    predictions_dict[prediction["name"]] = prediction["prediction"]
+
+actuals_dict = {}
+for actual in actuals:
+    actuals_dict[actual["name"]] = actual["actual"]
+
+results = []
+for (name, actual) in actuals_dict.items():
+    results.append(1 if predictions_dict[name] == actual else 0)
+
+accuracy = np.array(results).mean()
+
+print("Model Accuracy:", accuracy)
